4e3ae3a4b2ba2958be086e8634c6e8cbf3348f35
Sruthi Ramachandran <sruthiram.mp@gmail.com>
2022-05-08 20:55:46 -0400

    Added Imports to FileCompare.py

---
+++ Scripts/python/FileCompare.py
@@ -0,0 +1,12 @@
+# import section
+
+# Author Section
+# Program Name: FileCompare.py
+# Created by Keshav at 08/05/22 8:53 p.m.                       
+
+
+# Write your code here
+
+
+if __name__ == '__main__':
+    pass
